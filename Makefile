f2b40499f7e11c5f1aa65b5911bd63318c2d050e
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-05-29 08:19:00 +0000

    chore: Add gencalc target to Makefile for generating calculator protobuf files

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 gengrt:
 	protoc --go_out=./greet --go-grpc_out=./greet greet/greetpb/greet.proto
 
-.PHONY: gengrt
+gencalc:
+	protoc --go_out=./calculator --go-grpc_out=./calculator calculator/calculatorpb/calculator.proto
+
+.PHONY: gengrt gencalc
---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/lordofthemind/course_gRPC_Go/calculator/calculatorpb"
+	"github.com/lordofthemind/course_gRPC_Go/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	greetpb.UnimplementedGreetServiceServer
+}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	fmt.Printf("Received Sum RPC: %v\n", req)
+	firstNumber := req.GetFirstNumber()
+	secondNumber := req.GetSecondNumber()
+	result := firstNumber + secondNumber
+	res := &calculatorpb.SumResponse{
+		SumResult: result,
+	}
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Hello from calculator server")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	defer lis.Close()
+
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
